ebd9bfb3054ec28efb53ddfdbe4193150a955cc1
michal.lipski <kiwaczki@gmail.com>
2020-07-17 11:33:24 +0200

    first steps implemented

--- src/main/java/snow/SnowRescueService.java
+++ src/main/java/snow/SnowRescueService.java
@@ -2,14 +2,42 @@ package snow;
 
 import snow.dependencies.MunicipalServices;
 import snow.dependencies.PressService;
+import snow.dependencies.SnowplowMalfunctioningException;
 import snow.dependencies.WeatherForecastService;
 
 public class SnowRescueService {
 
-	public SnowRescueService(WeatherForecastService weatherForecastService, MunicipalServices municipalServices, PressService pressService) {
-	}
+    private final WeatherForecastService weatherForecastService;
+    private final MunicipalServices municipalServices;
+    private final PressService pressService;
 
-	public void checkForecastAndRescue() {
-	}
+    public SnowRescueService(WeatherForecastService weatherForecastService, MunicipalServices municipalServices, PressService pressService) {
+        this.weatherForecastService = weatherForecastService;
+        this.municipalServices = municipalServices;
+        this.pressService = pressService;
+    }
+
+    public void checkForecastAndRescue() {
+        if (weatherForecastService.getAverageTemperatureInCelsius() < 0) {
+            municipalServices.sendSander();
+        }
+
+        if (weatherForecastService.getSnowFallHeightInMM() > 3) {
+            tryToSendSnowplow();
+        }
+
+        if (weatherForecastService.getSnowFallHeightInMM() > 5) {
+            tryToSendSnowplow();
+        }
+
+    }
+
+    private void tryToSendSnowplow() {
+        try {
+            municipalServices.sendSnowplow();
+        } catch (SnowplowMalfunctioningException e) {
+            municipalServices.sendSnowplow();
+        }
+    }
 
 }
--- src/test/java/snow/SnowRescueServiceTest.java
+++ src/test/java/snow/SnowRescueServiceTest.java
@@ -1,4 +1,93 @@
 package snow;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import snow.dependencies.MunicipalServices;
+import snow.dependencies.SnowplowMalfunctioningException;
+import snow.dependencies.WeatherForecastService;
+
+import static org.mockito.Mockito.*;
+
 public class SnowRescueServiceTest {
+
+    private WeatherForecastService weatherForecastService;
+    private MunicipalServices municipalServices;
+    private SnowRescueService snowRescueService;
+
+    @BeforeEach
+    void setUp() {
+        weatherForecastService = Mockito.mock(WeatherForecastService.class);
+        municipalServices = Mockito.mock(MunicipalServices.class);
+        snowRescueService = new SnowRescueService(weatherForecastService, municipalServices, null);
+    }
+
+    @Test
+    void sends_sander_when_temperature_below_zero() {
+        havingTemperature(-1);
+
+        snowRescueService.checkForecastAndRescue();
+
+        verify(municipalServices).sendSander();
+    }
+
+    @Test
+    void do_not_send_sander_when_temperature_above_or_equal_zero() {
+        havingTemperature(0);
+
+        snowRescueService.checkForecastAndRescue();
+
+        verify(municipalServices, Mockito.never()).sendSander();
+    }
+
+    @Test
+    void sends_snowplow_when_snowing() {
+        havingSnowfall(4);
+
+        snowRescueService.checkForecastAndRescue();
+
+        verify(municipalServices).sendSnowplow();
+    }
+
+    @Test
+    void do_not_sends_snowplow_when_small_snow() {
+        havingSnowfall(3);
+
+        snowRescueService.checkForecastAndRescue();
+
+        verify(municipalServices,never()).sendSnowplow();
+    }
+
+    @Test
+    void sends_two_snowplow_when_a_lot_of_snowing() {
+        havingSnowfall(6);
+
+        snowRescueService.checkForecastAndRescue();
+
+        verify(municipalServices,times(2)).sendSnowplow();
+    }
+
+    @Test
+    void sends_second_snowplow_when_first_failed() {
+        havingSnowfall(4);
+        whenFirstSnowplowFails();
+
+        snowRescueService.checkForecastAndRescue();
+
+        verify(municipalServices, times(2)).sendSnowplow();
+    }
+
+    private void whenFirstSnowplowFails() {
+        doThrow(SnowplowMalfunctioningException.class).doNothing().when(municipalServices).sendSnowplow();
+    }
+
+    private void havingTemperature(int temperature) {
+        Mockito.when(weatherForecastService.getAverageTemperatureInCelsius()).thenReturn(temperature);
+    }
+
+    private void havingSnowfall(int snowAmount) {
+        Mockito.when(weatherForecastService.getSnowFallHeightInMM()).thenReturn(snowAmount);
+    }
+
+
 }
